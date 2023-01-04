b4cd16992f998a963948f3819cfcc17c7b5ae284
Yuri Burger <yuriburger@outlook.com>
2023-01-04 10:21:07 +0100

    Initial setup

---
+++ src/main/java/com/vxcompany/graphstoreapi/GraphstoreapiApplication.java
@@ -0,0 +1,13 @@
+package com.vxcompany.graphstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphstoreapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphstoreapiApplication.class, args);
+	}
+
+}
